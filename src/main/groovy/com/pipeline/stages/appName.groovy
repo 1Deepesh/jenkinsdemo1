ebbdf5cdbbf3180f4028c27968827026d098e679
1Deepesh <56162357+1Deepesh@users.noreply.github.com>
2022-10-03 22:20:03 +0530

    Update appName.groovy

--- src/main/groovy/com/pipeline/stages/appName.groovy
+++ src/main/groovy/com/pipeline/stages/appName.groovy
@@ -1,7 +1,9 @@
 def appName() {
+
         gitURL = "https://github.com/1Deepesh/jenkinsdemo1.git"
-        appName = ((git.URL).split("/")[1]).toString()
-        println (appName)
+        appName = (((gitURL).split("/")[4]).split(".git")[0]).toString()
+        println ("We have found the appname for you: " + appName)
+        return appName
         
 }
 
